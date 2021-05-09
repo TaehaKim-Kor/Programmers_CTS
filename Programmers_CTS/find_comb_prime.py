1dacce416a9442df27697119b737cac05489fd98
TaehaKim-Kor <anstnrltk@gmail.com>
2021-05-09 23:45:56 +0900

    Coding Test Practice

--- Programmers_CTS/find_comb_prime.py
+++ Programmers_CTS/find_comb_prime.py
@@ -3,7 +3,7 @@ def primenum_classifier(tgt):
     if tgt == 1:
         return True
     else:
-        for i in range(2,tgt-1):
+        for i in range(2,tgt):
             if tgt % i == 0:
                 return False
     return True
@@ -31,9 +31,10 @@ def dwsearcher(numlist, visit, anslist, current_position):
     visit[current_position] = 1
     trial = trial_counter(visit)
     if trial < 3:
-        for j in range(len(numlist)):
+        for j in range(len(visit)):
             if visit[j] == 0:
                 dwsearcher(numlist,visit,anslist,j)  
+        visit[current_position] = 0 #여기선 visit 초기화를 해야되는데 network 문제에선 왜 안해도 됬는 지를 모르겠다..
     elif trial == 3:
         tgt = tgt_adder(numlist, visit)
         if primenum_classifier(tgt):
@@ -42,6 +43,7 @@ def dwsearcher(numlist, visit, anslist, current_position):
                 if visit[j] == 1:
                     ans.append(numlist[j])
             anslist.append(ans)
+        visit[current_position] = 0
                 
 def findcomb(nums):
     answer_list = []
--- Programmers_CTS/main.py
+++ Programmers_CTS/main.py
@@ -3,7 +3,7 @@ import find_comb_prime as fcp
 import network as nl
 #fp.findperson(5,[2,4],[1,3,5])
 fcp.findcomb([1,2,3,4])
-# fcp.findcomb([1,2,7,6,4])
+fcp.findcomb([1,2,7,6,4])
 # print(fcp.findcomb([1,2,3,4]))
 # print(fcp.findcomb([1,2,7,6,4]))
-print(nl.netlink(3,[[1,1,0],[1,1,0],[0,0,1]]))
+# print(nl.netlink(3,[[1,1,0],[1,1,0],[0,0,1]]))
