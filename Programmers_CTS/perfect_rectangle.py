20447b9e6455906fe49d84ddc52b1b9516f12c7c
VCL-C01 <VCL@seoultech.ac.kr>
2021-05-13 17:46:09 +0900

    Coding Test Practice

--- Programmers_CTS/perfect_rectangle.py
+++ Programmers_CTS/perfect_rectangle.py
@@ -5,18 +5,28 @@ def compressor(w,h):
         if w % i == 0 and h % i == 0:
             return int(w/i),int(h/i),True
     return w,h,False
+def Euclidean_compressor(w,h):
+    #https://coding-factory.tistory.com/599
+    if min(w,h) == 0:
+        return int(max(w,h))
+    else:
+        return Euclidean_compressor(min(w,h),max(w,h)%min(w,h))
 def finder(w,h):
     grad = w/h
     minus = 0
     for i in range(1,h):
         minus += w-math.ceil(grad*i)
-        print(minus)
     return w*h-minus*2
 def solution(w,h):
+    state=1
     ow , oh = w , h
     answer = ow*oh
-    s = True
-    while(s):
-        w,h,s=compressor(w,h)
-    answer -= int(finder(w,h))*int(ow/w)
+    if state == 0:
+        s = True
+        while(s):
+           w,h,s=compressor(w,h)
+        answer -= int(finder(w,h))*int(ow/w)
+    else:
+        ratio = Euclidean_compressor(w,h)
+        answer -= int(finder(int(w/ratio),int(h/ratio)))*int(ratio)
     return answer
