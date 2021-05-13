32fc3ccc1f74074fe2f9b1c18dc8585d4254cf4f
VCL-C01 <VCL@seoultech.ac.kr>
2021-05-13 16:49:40 +0900

    Coding Test Practice

--- Programmers_CTS/perfect_rectangle.py
+++ Programmers_CTS/perfect_rectangle.py
@@ -1,34 +1,22 @@
 #https://programmers.co.kr/learn/courses/30/lessons/62048?language=python3
 import math
 def compressor(w,h):
-    for i in range(2,min(w,h)):
+    for i in range(2,math.floor(min(w/2,h/2))):
         if w % i == 0 and h % i == 0:
             return int(w/i),int(h/i),True
     return w,h,False
-        
 def finder(w,h):
     grad = w/h
-    tgt = [0]
     minus = 0
-    for i in range(h):
-        tgt.append(grad*i)
-    for i in range(1,len(tgt)):
-        btgt = math.floor(tgt[i-1])
-        atgt = math.floor(tgt[i])
-        minus += atgt - btgt + 1
-    return minus
-def finder2(w,h):
-    if w<h:
-        return 2*h-2
-    elif w>h:
-        return 2*h-1
-    else:
-        return h
+    for i in range(1,h):
+        minus += w-math.ceil(grad*i)
+        print(minus)
+    return w*h-minus*2
 def solution(w,h):
     ow , oh = w , h
     answer = ow*oh
     s = True
     while(s):
         w,h,s=compressor(w,h)
-    answer -= int(finder2(w,h))*int(ow/w)
+    answer -= int(finder(w,h))*int(ow/w)
     return answer
