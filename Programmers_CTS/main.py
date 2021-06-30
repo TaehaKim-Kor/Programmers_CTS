cc46ac4a8759796bcfff04f57587d38738f8a711
VCL-C01 <VCL@seoultech.ac.kr>
2021-06-30 14:53:25 +0900

    Naver Webtoon Coding Test Practice 210630

--- Programmers_CTS/main.py
+++ Programmers_CTS/main.py
@@ -4,9 +4,10 @@ import network as nl
 import rotating as rt
 import pair_remove as pr
 import perfect_rectangle as pr2
-#fp.findperson(5,[2,4],[1,3,5])
-#fcp.findcomb([1,2,3,4])
-#fcp.findcomb([1,2,7,6,4])
+import nvweb as nv
+# fp.findperson(5,[2,4],[1,3,5])
+# fcp.findcomb([1,2,3,4])
+# fcp.findcomb([1,2,7,6,4])
 # print(fcp.findcomb([1,2,3,4]))
 # print(fcp.findcomb([1,2,7,6,4]))
 # print(nl.netlink(3,[[1,1,0],[1,1,0],[0,0,1]]))
@@ -19,5 +20,7 @@ import perfect_rectangle as pr2
 # print(pr.solution("abccccbaaa"))
 # print(pr.solution("abccaabaa"))
 # print(pr.solution("a"))
-
-print(pr2.solution(8,12))
+# print(nv.solution0([[1,4],[3,4],[3,10]]))
+# print(nv.solution0([[1,1],[2,2],[1,2]]))
+# print(nv.solution0([[1,3],[7,9],[1,9]]))
+# print(pr2.solution(8,12))
--- Programmers_CTS/nvweb.py
+++ Programmers_CTS/nvweb.py
@@ -1,15 +1,35 @@
+# Temporal test
+def solution0(v):
+    pa = v #point_array
+    answer = [0,0]
+    for i in range(2):
+        va = [] #void_array
+        for j in range(3):
+            va.append(pa[j][i])
+        va.sort()
+        if va[0] != va[1]:
+            answer[i] += va[0]
+        else:
+            answer[i] += va[2]
+    return answer
+
 # test1
 def solution1():
-
+    answer =[]
+    return answer
 
 
 # test2
 def solution2():
+    answer =[]
+    return answer
 
 
 
 # test3
 def solution3():
+    answer =[]
+    return answer
 
 
 
