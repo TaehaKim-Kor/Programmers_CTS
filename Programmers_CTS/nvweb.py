3477bac8ee984638945f600a1f64792af139335d
VCL-C01 <amvision1248@gmail.com>
2021-07-04 17:46:19 +0900

    Naver Webtoon Coding Test 210704 14:00~16:00

--- Programmers_CTS/nvweb.py
+++ Programmers_CTS/nvweb.py
@@ -14,24 +14,78 @@ def solution0(v):
     return answer
 
 # test1
-def solution1():
-    answer =[]
+def checker(S, pattern):
+    unique_p= ''.join(set(pattern))
+    charlist = [0 for i in range(len(unique_p))]
+    charcheck = [0 for i in range(len(unique_p))]
+    divlist = [0 for i in range(len(unique_p))]
+    for i in range(len(pattern)):
+        for j in range(len(unique_p)):
+            if pattern[i]==unique_p[j]: charcheck[j] += 1
+    for i in range(len(S)):
+        for j in range(len(unique_p)):
+            if S[i]==unique_p[j]: charlist[j] += 1
+    for i in range(len(unique_p)):
+        divlist[i] = charlist[i]//charcheck[i]
+    answer = min(divlist)
+    return answer
+def solution1(S, pattern):
+    answer = 0
+    for i in range(len(S)-len(pattern)+1):
+        tgt = S[i:i+len(pattern)]
+        answer += checker(tgt,pattern)
     return answer
 
 
 # test2
-def solution2():
-    answer =[]
-    return answer
-
-
+def searcher2(tgt,al,fwl,wl,current):
+    if len(tgt)==0:
+        tgt.append(fwl[current])
+    else:
+        tgt.append(wl[current])
+    print(tgt,len(tgt))
+    if len(tgt) == 5:
+        mychar = ''
+        for i in range(5):
+            mychar += tgt[i]
+        al.append(mychar)
+        tgt.pop()
+    else:
+        for i in range(len(wl)):
+            print(current,i)
+            searcher(tgt,al,fwl,wl,i)
+            tgt.pop()
+def solution2(word):
+    alist = []
+    fwordlist = ["A","E","I","O","U"]
+    wordlist = ["","A","E","I","O","U"]
+    count = 0
+    tgt = []
+    for i in range(len(fwordlist)):
+        searcher2(tgt,alist,fwordlist,wordlist,i)
+        tgt.pop()
+    alist.sort()
+    return alist.index(word)+1
 
 # test3
-def solution3():
-    answer =[]
-    return answer
-
-
-
-
-    
+def searcher(current, letter, visit, plist, k):
+    visit[current] = 1
+    if sum(visit) >= k:
+        char = ''
+        for i in range(len(visit)):
+            if visit[i] == 1:
+                char += letter[i]
+        plist.append(char)
+        visit[current]=0
+    else:
+        for i in range(current+1,len(letter)):
+            searcher(i,letter,visit,plist,k)
+            visit[i]=0
+def solution(letters, k):
+    visit = [0 for i in range(len(letters))]
+    plist = []
+    for i in range(len(letters)):
+        searcher(i,letters,visit,plist,k)
+        visit[i]=0
+    plist.sort(reverse=True)
+    return plist[0]
